d5f9db4e811d0579d77fc909b355237488c262a3
leiyongcheng <leiyongcheng@ymt360.com>
2020-12-09 19:01:17 +0800

    添加main.go

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("xxxx")
+	// send()
+
+}
+
+func send() {
+	url := "http://sc.ftqq.com/SCU124621T9af583eb31c71ee1ac579fe2db1fbeab5fa7fcd8a0079.send"
+	method := "POST"
+
+	payload := strings.NewReader(`text=22`)
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Add("Connection", "keep-alive")
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+}
+
