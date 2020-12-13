f3c30eae94c58678ee84c3e12dc362f03bb14f40
leiyongcheng <leiyongcheng@ymt360.com>
2020-12-13 10:53:32 +0800

    企业微信

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # arg=`ifconfig eth0 | grep 'inet' | awk '{print $2}' | awk -F ':' '{print $2}'`
-if [  -n $MSG_SCKEY ] ;then
-go run main.go $MSG_SCKEY
+if [  -n $WX_SECURITYKEY ] ;then
+go run main.go $WX_SECURITYKEY
 fi
