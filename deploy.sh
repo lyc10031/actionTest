a47cf71ca7ab9866f7bacc04b4b2ae0c3815e6a8
lyc10031 <32125389+lyc10031@users.noreply.github.com>
2021-06-14 14:11:24 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # arg=`ifconfig eth0 | grep 'inet' | awk '{print $2}' | awk -F ':' '{print $2}'`
-if [  -n $WX_SECURITYKEY ];then
+if [ -n $WX_SECURITYKEY ];then
 go run main.go $WX_SECURITYKEY
 fi
